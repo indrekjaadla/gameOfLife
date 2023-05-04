50a66ecb427145f3201db12bce14ed4e5276cd19
indrekjaadla <indrekjaadla@gmail.com>
2023-05-04 10:26:15 +0300

    () for filling an array with predefined elements TDD () for generating an empty field for GameOfLife TDD

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,3 +1,19 @@
-export default function sum(...nums) {
-    return nums.reduce((a, b) => a + b, 0)
+//TODO web application for Conway's Game of Life.
+//TODO TDD of game logic in JS
+//TODO visualizing - black/white table
+//TODO initial pattern hardcoded blinker
+//TODO user can choose between predefined patterns
+
+export function arrayFiller(arrayToFill, fillingElement) {
+    for (let i = 0; i < arrayToFill.length; i++) {
+        arrayToFill[i] = fillingElement
+    }
+}
+
+export function generateEmptyField(nOfRows, nOfColumns) {
+    let a = new Array(nOfRows)
+    let b = new Array(nOfColumns)
+    arrayFiller(b, 0);
+    arrayFiller(a, b);
+    return a;
 }
--- src/test/helpers.test.js
+++ src/test/helpers.test.js
@@ -1,10 +1,46 @@
 import {expect, test} from "vitest";
-import sum from "src/helpers/helpers.js"
+// import { assertType, expectTypeOf } from 'vitest'
+import {arrayFiller, generateEmptyField} from "src/helpers/helpers.js"
 
-test('1 + 1', () => {
-    expect(sum(1, 1)).equal(2)
+test('arrayFillerNumber', () => {
+    let arrayLength = 5
+    let arrayToFill = new Array(arrayLength)
+    let fillingElement = 666
+    arrayFiller(arrayToFill, fillingElement)
+    expect(arrayToFill[0]).equal(fillingElement)
+    expect(arrayToFill[arrayLength - 1]).equal(fillingElement)
+    console.log(arrayToFill[arrayLength - 1])
 })
 
-test('1 + 2 + 3', () => {
-    expect(sum(1, 2, 3)).equal(6)
+test('arrayFillerString', () => {
+    let arrayLength = 5
+    let arrayToFill = new Array(arrayLength)
+    let fillingElement = 'testString'
+    arrayFiller(arrayToFill, fillingElement)
+    expect(arrayToFill[0]).equal(fillingElement)
+    expect(arrayToFill[arrayLength - 1]).equal(fillingElement)
+    console.log(arrayToFill[arrayLength - 1])
+})
+
+test('arrayFillerArray', () => {
+    let arrayLength = 5
+    let arrayToFill = new Array(arrayLength)
+    let fillingElement = []
+    arrayFiller(arrayToFill, fillingElement)
+    expect(arrayToFill[0]).equal(fillingElement)
+    expect(arrayToFill[arrayLength - 1]).equal(fillingElement)
+    console.log(arrayToFill[arrayLength - 1])
+})
+
+test('emptyField', () => {
+    let nOfRows = 6
+    let nOfColumns = 6
+    expect(generateEmptyField(nOfRows, 10).length).equal(nOfRows)
+    expect(generateEmptyField(nOfRows, nOfColumns)[0].length).equal(nOfColumns)
+
+    expect(generateEmptyField(nOfRows, nOfColumns)[0][0]).equal(0)
+    expect(generateEmptyField(nOfRows, nOfColumns)[0][nOfColumns - 1]).equal(0)
+
+    expect(generateEmptyField(nOfRows, nOfColumns)[nOfRows - 1][0]).equal(0)
+    expect(generateEmptyField(nOfRows, nOfColumns)[nOfRows - 1][nOfColumns - 1]).equal(0)
 })

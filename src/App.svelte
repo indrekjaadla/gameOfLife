eb5a9ee85e2cf1c0449c24203d496a8bf72e8999
indrekjaadla <indrekjaadla@gmail.com>
2023-05-05 14:46:49 +0300

    UI initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,28 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
+
+import GameField from "./components/GameField.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <h1>GAME OF LIFE</h1>
+    <div class="flex-container">
+        <button>Blinker</button>
+        <button>Toad</button>
+        <button>Beacon</button>
+    </div>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <form class="flex-container">
+        <input type="number" placeholder="South" min="3" max="50">
+        <input type="number" placeholder="East" min="3" max="50">
+        <input type="submit" value="Create Field">
+    </form>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+<GameField/>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>

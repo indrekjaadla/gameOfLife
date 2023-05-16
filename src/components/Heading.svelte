737173feadfba056782ff0cc3ae991652da7f88c
indrekjaadla <indrekjaadla@gmail.com>
2023-05-16 13:47:23 +0300

    h1 styling moved to Heading.svelte

--- src/components/Heading.svelte
+++ src/components/Heading.svelte
@@ -1,3 +1,11 @@
 <div class="flex-container-row">
     <h1>GAME OF LIFE</h1>
 </div>
+
+<style>
+    h1 {
+        margin: 0;
+        font-size: 4rem;
+        align-content: center;
+    }
+</style>
